d1c4bfe36919ec0acc9d04e0690c65e92a474dec
Andrey Tuktam <tuktam@mail.ru>
2022-07-17 18:41:29 +0700

    Home_work_7S

---
+++ 50/Program.cs
@@ -0,0 +1,23 @@
+﻿int M = 5;
+int N = 5;
+int[,] array = new int[M, N];
+Random rnd = new Random();
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        array[i, j] = rnd.Next(1, 10);
+        Console.Write(array[i, j] + " ");
+    } 
+    Console.WriteLine();   
+}
+int k = Convert.ToInt32(Console.ReadLine());
+int l = Convert.ToInt32(Console.ReadLine());
+if (k > M - 1 || k < 0 || l > N - 1 || l < 0)
+{
+    Console.WriteLine("Нет такого эллемента в массиве!");
+} else {
+    Console.Write(array[k, l]);
+}
+
+
---
+++ 52/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("введите колличество строк - i: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите колличество столбцов - j: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[M, N];
+Random rnd = new Random();
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        array[i, j] = rnd.Next(1, 10);
+        Console.Write(array[i, j] + " "); 
+    } 
+    Console.WriteLine(); 
+}
+for (int j = 0; j < N; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < M; i++)
+    {
+        sum = array[i, j] + sum;
+    } 
+    Console.WriteLine($"среднее арифметическое j = {j}: {sum / M}"); 
+}
+
