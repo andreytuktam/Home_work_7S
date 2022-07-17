6e3761dcf6d5b6a59ff8e0e03cf773f70c1fbf7e
Andrey Tuktam <tuktam@mail.ru>
2022-07-17 18:47:02 +0700

    Home_work_7S

--- 47/Program.cs
+++ 47/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
 int M = Convert.ToInt32(Console.ReadLine());
 int N = Convert.ToInt32(Console.ReadLine());
 double[,] array = new double[M, N];
